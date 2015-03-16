54ff27a2d7c77ac77f6265c93cc1c7f42dfe41bc
rreddyc <radhakrishnareddy25@gmail.com>
2015-03-16 17:56:32 -0400

    Know the facts
    BMI
    Health Tips

--- app/src/main/java/com/sw551/fairfield/healthcheq/know_the_facts_child1.java
+++ app/src/main/java/com/sw551/fairfield/healthcheq/know_the_facts_child1.java
@@ -2,23 +2,27 @@ package com.sw551.fairfield.healthcheq;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class know_the_facts_child1 extends ActionBarActivity {
-
+    TextView textView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_know_the_facts_child1);
+        textView=(TextView) findViewById(R.id.textView3);
+        textView.setText(Html.fromHtml("<p>Health consequences of overweight and obesity in adults[edit] The BMI ranges are based on the relationship between body weight and disease and death. Overweight and obese individuals are at an increased risk for many diseases and health conditions, including the following:  Hypertension Dyslipidemia (for example, high LDL cholesterol, low HDL cholesterol, or high levels of triglycerides) Type 2 diabetes Coronary heart disease Stroke Gallbladder disease Osteoarthritis Sleep apnea and respiratory problems At least 10 cancers (including endometrial, breast, and colon). </p>"));
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_know_the_facts, menu);
+        getMenuInflater().inflate(R.menu.menu_know_the_facts_child1, menu);
         return true;
     }
 
@@ -28,6 +32,7 @@ public class know_the_facts_child1 extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {

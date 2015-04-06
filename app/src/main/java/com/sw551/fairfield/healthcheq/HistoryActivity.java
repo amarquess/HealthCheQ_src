7e36acfc715ef3457e9f23bbd4f917b1f1ecf481
rreddyc <radhakrishnareddy25@gmail.com>
2015-04-06 13:49:22 -0400

    History Know the facts
    check and sync

--- app/src/main/java/com/sw551/fairfield/healthcheq/HistoryActivity.java
+++ app/src/main/java/com/sw551/fairfield/healthcheq/HistoryActivity.java
@@ -5,13 +5,40 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.PointLabeler;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import java.util.Arrays;
+
 
 public class HistoryActivity extends ActionBarActivity {
 
+    private XYPlot plot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        plot=(XYPlot)findViewById(R.id.mySimpleXYPlot);
+        Number[] series1={10,8,6,4,2};
+        Number[] series2={4,6,3,8,2};
+        XYSeries xys1=new SimpleXYSeries(Arrays.asList(series1),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"Weight");
+        XYSeries xys2=new SimpleXYSeries(Arrays.asList(series2),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"Weight");
+        LineAndPointFormatter sf1=new LineAndPointFormatter();
+        sf1.setPointLabelFormatter(new PointLabelFormatter());
+        sf1.configure(getApplicationContext(),R.xml.line_plf1);
+        plot.addSeries(xys1,sf1);
+        LineAndPointFormatter sf2=new LineAndPointFormatter();
+        sf2.setPointLabelFormatter(new PointLabelFormatter());
+        sf2.configure(getApplicationContext(),R.xml.line_plf2);
+        plot.addSeries(xys2,sf2);
+        plot.setTicksPerRangeLabel(3);
+        plot.getGraphWidget().setDomainLabelOrientation(-45);
+
+
     }
 
 

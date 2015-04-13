8a2512e8a7e3e79c4a44dfc5c2ddabcd8f85d9b9
rreddyc <radhakrishnareddy25@gmail.com>
2015-04-13 19:32:06 -0400

    History
    history java and xml

--- app/src/main/java/com/sw551/fairfield/healthcheq/HistoryActivity.java
+++ app/src/main/java/com/sw551/fairfield/healthcheq/HistoryActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
@@ -12,11 +14,15 @@ import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 
 public class HistoryActivity extends ActionBarActivity {
-
+private Button button;
     private XYPlot plot;
     SqlDbHelper db = new SqlDbHelper(this);
     @Override
@@ -24,10 +30,20 @@ public class HistoryActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         plot=(XYPlot)findViewById(R.id.mySimpleXYPlot);
-        Number[] series1={10,8,6,4,2};
-        Number[] series2={4,6,3,8,2};
-        XYSeries xys1=new SimpleXYSeries(Arrays.asList(series1),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"goal");
-        XYSeries xys2=new SimpleXYSeries(Arrays.asList(series2),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"me");
+        button=(Button)findViewById(R.id.button_clear);
+        ArrayList<Record> recordList=db.getAllRecord(1);
+        Number[] series1=new Integer[recordList.size()];
+        Number[] series2=new Integer[recordList.size()];
+        for (int i = 0; i < recordList.size(); i++) {
+            long foo = Long.parseLong(recordList.get(i).getDate());
+            Date date = new Date(foo);
+            DateFormat formatter = new SimpleDateFormat("MMdd");
+            series1[i]=Integer.parseInt(formatter.format(date));
+            System.out.println(Integer.parseInt(formatter.format(date)));
+            series2[i]=(int)recordList.get(i).getWeight();
+        }
+        final XYSeries xys1=new SimpleXYSeries(Arrays.asList(series1),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"goal");
+        final XYSeries xys2=new SimpleXYSeries(Arrays.asList(series2),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"me");
         LineAndPointFormatter sf1=new LineAndPointFormatter();
         sf1.setPointLabelFormatter(new PointLabelFormatter());
         sf1.configure(getApplicationContext(),R.xml.line_plf1);
@@ -36,9 +52,20 @@ public class HistoryActivity extends ActionBarActivity {
         sf2.setPointLabelFormatter(new PointLabelFormatter());
         sf2.configure(getApplicationContext(),R.xml.line_plf2);
         plot.addSeries(xys2,sf2);
-        plot.setTicksPerRangeLabel(3);
-        plot.getGraphWidget().setDomainLabelOrientation(-45);
+        plot.setTicksPerRangeLabel(1);
+        plot.getGraphWidget().setDomainLabelOrientation(3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                plot.clear();
+                plot.removeSeries(xys1);
+                plot.removeSeries(xys2);
+                plot.redraw();
+                db.deleteTable12(1);
 
+
+            }
+        });
 
     }
 
